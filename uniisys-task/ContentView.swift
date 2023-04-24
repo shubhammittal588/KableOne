6b64d20c9e945cee07fe0144e08e8442327c2c63
Shubham Mittal <shubhammittal588@gmail.com>
2023-04-24 11:15:48 +0530

    Initial Commit

---
+++ uniisys-task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uniisys-task
+//
+//  Created by Shubham Mittal on 24/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uniisys-task/uniisys_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  uniisys_taskApp.swift
+//  uniisys-task
+//
+//  Created by Shubham Mittal on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct uniisys_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
